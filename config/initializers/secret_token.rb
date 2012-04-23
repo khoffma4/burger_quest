9994b3fb7e6139b73f3fed0d508c8d3fc7253704
Kevin Hoffman <khoffma4@gmail.com>
2012-04-22 19:05:55 -0500

    starts new rails app for burgerquest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BurgerQuest::Application.config.secret_token = 'afc99f8fa651c11057f14e0722c74376c945151a97506aba193f9fb061be2f77c350b34b6bc9aa5d61835aaf41525f70c43a738f355be6e8b0568d3f84210fe3'
