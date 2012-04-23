a8bb05b36b49231582258787a9bd152e9b2e14c1
Kevin Hoffman <khoffma4@gmail.com>
2012-04-22 19:28:22 -0500

    configures environment files per devise install instructions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ BurgerQuest::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Devise install said to add this here
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ BurgerQuest::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Devise install said to add this, edit when domain is purchased
+  config.action_mailer.default_url_options = { :host => 'www.example.com' }
+  
 end
