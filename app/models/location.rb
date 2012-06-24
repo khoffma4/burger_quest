56506ecc57f1bc108c8dc31bc604e511790296e8
Kevin Hoffman <khoffma4@gmail.com>
2012-06-24 13:29:06 -0500

    sets up Location model to be used with gmaps

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,16 @@
 class Location < ActiveRecord::Base
   attr_accessible :city, :state, :street, :zip, :restaurant_id
-  
+  acts_as_gmappable
+
   belongs_to :restaurant
+
+  def gmaps4rails_address
+  #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
+    "#{self.street}, #{self.city}, #{self.state} #{self.zip}" 
+  end
   
+  def gmaps4rails_infowindow
+    "#{self.restaurant.name}"
+  end
+
 end
